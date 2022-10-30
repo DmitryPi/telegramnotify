260371914cd58444f1805c6f3fe71bc972ce840d
Dmitry G <sdprecovery@gmail.com>
2022-10-30 11:42:03 +0300

    update: telegram TestUsers test_build_user_data, test_build_user_data_admin

--- telegram/modules/tests/test_users.py
+++ telegram/modules/tests/test_users.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from ..users import UserRole, build_user
+from ..users import PremiumStatus, UserRole, build_user
 
 
 class TestUsers(TestCase):
@@ -19,4 +19,22 @@ class TestUsers(TestCase):
         assert user.username == self.user_tg["username"]
         assert user.first_name == self.user_tg["first_name"]
         assert user.role == UserRole.USER.value
+        assert user.services == ""
+        assert user.bill == 0.0
+        assert user.wallet == 0.0
+        assert user.premium_status == PremiumStatus.TRIAL.value
+        assert user.premium_expire == ""
+        assert isinstance(user.created, str)
+
+    def test_build_user_data_admin(self):
+        user = build_user(self.user_tg, admin=True)
+        assert user.uid == self.user_tg["id"]
+        assert user.username == self.user_tg["username"]
+        assert user.first_name == self.user_tg["first_name"]
+        assert user.role == UserRole.ADMIN.value
+        assert user.services == ""
+        assert user.bill == 0.0
+        assert user.wallet == 0.0
+        assert user.premium_status == PremiumStatus.TRIAL.value
+        assert user.premium_expire == ""
         assert isinstance(user.created, str)
