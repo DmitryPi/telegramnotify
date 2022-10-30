e769560a45a47672434fe006078fff4082e16c7a
Dmitry G <sdprecovery@gmail.com>
2022-10-30 11:05:37 +0300

    Update: telegram users.py PremiumStatus(enum); User dataclass, build_user()

--- telegram/modules/users.py
+++ telegram/modules/users.py
@@ -8,27 +8,40 @@ class UserRole(Enum):
     ADMIN = "Админ"
 
 
+class PremiumStatus(Enum):
+    TRIAL = "Пробный период"
+    GENERIC = "Обычный"
+    GIGAPREMIUM = "ГИГАПремиум"
+
+
 @dataclass
 class User:
     uid: int
     username: str
     first_name: str
     role: UserRole
+    bill: float
+    wallet: float
+    services: str  # json field
+    premium: PremiumStatus
+    premium_expire: str
     created: str
-    updated: str
 
 
 def build_user(tg_data: dict, admin=False) -> User:
     """user_data - из json файла; tg_data - объект телеграм пользователя"""
-    now = str(datetime.now())
     username = tg_data["username"] if tg_data["username"] else tg_data["first_name"]
     user_role = UserRole.ADMIN.value if admin else UserRole.USER.value
     user = User(
-        tg_data["id"],
-        username,
-        tg_data["first_name"],
-        user_role,
-        now,
-        now,
+        uid=tg_data["id"],
+        username=username,
+        first_name=tg_data["first_name"],
+        role=user_role,
+        services="",
+        bill=0.0,
+        wallet=0.0,
+        premium_status=PremiumStatus.TRIAL,
+        premium_expire="",
+        created=str(datetime.now()),
     )
     return user
