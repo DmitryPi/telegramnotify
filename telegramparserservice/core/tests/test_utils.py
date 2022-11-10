4703c45cc88b7fa12f9d67e376042f7bb05be51e
Dmitry G <sdprecovery@gmail.com>
2022-11-10 11:47:31 +0300

    Tests passed

--- telegramparserservice/core/tests/test_utils.py
+++ telegramparserservice/core/tests/test_utils.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from ..utils import datetime_days_ahead
 
 
-def test_datetime_days_ahead(self):
+def test_datetime_days_ahead():
     examples = [
         (datetime_days_ahead(0), datetime.now() + timedelta(days=0)),
         (datetime_days_ahead(3), datetime.now() + timedelta(days=3)),
