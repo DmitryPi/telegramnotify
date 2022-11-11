36a6457e3ffbc7f796029fec95cd732c2242d358
Dmitry G <sdprecovery@gmail.com>
2022-11-11 12:04:45 +0300

    Update: users User model

--- telegramservice/users/models.py
+++ telegramservice/users/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -11,11 +12,36 @@ class User(AbstractUser):
     check forms.SignupForm and forms.SocialSignupForms accordingly.
     """
 
-    #: First and last name do not cover name patterns around the globe
+    # choices
+    class PremiumStatus(models.TextChoices):
+        trial = "trial", _("Trial")
+        regular = "regular", _("Regular")
+        permanent = "permanent", _("Permanent")
+        expired = "expired", _("Expired")
+
+    # fields
     tg_id = models.IntegerField(_("Telegram ID"), unique=True, null=True, blank=True)
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    # service fields
+    services = ArrayField(models.CharField(max_length=55), blank=True, default=list)
+    words = ArrayField(models.CharField(max_length=112), blank=True, default=list)
+    # business logic fields
+    bill = models.DecimalField(_("Тариф"), max_digits=11, decimal_places=2, default=0)
+    wallet = models.DecimalField(
+        _("Баланс"), max_digits=11, decimal_places=2, default=0
+    )
+    premium_status = models.CharField(
+        _("Premium Status"),
+        max_length=55,
+        choices=PremiumStatus.choices,
+        default=PremiumStatus.expired,
+    )
+    premium_expire = models.DateTimeField(auto_now_add=True)
+    # datetime fields
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
