6ecc0f38770a1db3e9e96c84a317d72d5b5bf104
Dmitry G <sdprecovery@gmail.com>
2022-11-11 17:03:02 +0300

    update: core/models - init Order model

--- telegramservice/core/models.py
+++ telegramservice/core/models.py
@@ -1,3 +1,34 @@
-from django.db import models  # noqa skip
+import uuid as uuid_lib
 
-# Create your models here.
+from django.conf import settings
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
+
+
+class Order(TimeStampedModel):
+    # choices
+    class Status(models.TextChoices):
+        FAILED = "FAILED", _("Failed")
+        PENDING = "PENDING", _("Pending")
+        SUCCESS = "SUCCESS", _("Success")
+
+    # relations
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # fields
+    uuid = models.UUIDField(db_index=True, default=uuid_lib.uuid4, editable=False)
+    status = models.CharField(
+        max_length=55, choices=Status.choices, default=Status.PENDING
+    )
+    invoice_payload = models.CharField(max_length=55)
+    currency = models.CharField(max_length=5)
+    total_amount = models.DecimalField(max_digits=11, decimal_places=2)
+    telegram_payment_charge_id = models.CharField(max_length=100)
+    provider_payment_charge_id = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
+
+    def __str__(self):
+        return f"{self.status} : {self.uuid}"
