1916b8c7b1027e392aa6d5b41d029e346dd77da2
Dmitry G <sdprecovery@gmail.com>
2022-11-13 13:20:26 +0300

    Update: FLParser get_projects_info

--- telegramservice/core/parsers.py
+++ telegramservice/core/parsers.py
@@ -1,4 +1,5 @@
 from environ import Env
+from requests_html import HTMLSession
 
 from telegramservice.core.models import Target
 
@@ -9,13 +10,37 @@ class FLParser:
         self.target = ""
         self.set_target()
 
-    def set_target(self):
+    def set_target(self) -> None:
+        """Fl.ru target - https://www.fl.ru/projects/"""
         try:
             target = Target.objects.get(title="FL.ru")
-            self.target = target.url_body
+            self.target = target
         except Target.DoesNotExist as e:
             print("Target you are trying to find DoesNotExist")
             raise e
 
+    def get_projects_info(self) -> list[[int, str]]:
+        """GET target page => parse project id, build url
+
+        >> return [[proj_id, proj_url]]
+        """
+        session = HTMLSession()
+        response = session.get(self.target.url_query)
+        projects = response.html.find(".b-post__link")  # <a> proj link
+        projects_info = []
+        for proj in projects:
+            proj_id = int(proj.attrs["href"].split("/")[2])
+            # https://www.fl.ru/ + projects/<id>/<slug>
+            proj_url = self.target.url_body + proj.attrs["href"][1:]
+            projects_info.append([proj_id, proj_url])
+        return projects_info
+
+    def get_projects_data(self) -> dict:
+        pass
+
+    def save_project_data(self) -> None:
+        """Save project data if not exist"""
+        pass
+
     def run(self):
-        print(self.target)
+        print(self.get_projects_info())
