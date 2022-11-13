cd431dad5bca8f7e0ee9a572f47980f593def234
Dmitry G <sdprecovery@gmail.com>
2022-11-13 17:02:20 +0300

    Update utils: save_parser_entry, get_parser_entry; Refactoring: FLParser

--- telegramservice/core/parsers.py
+++ telegramservice/core/parsers.py
@@ -3,7 +3,8 @@ from collections import namedtuple
 from environ import Env
 from requests_html import HTMLSession
 
-from .models import ParserEntry, Target
+from .models import Target
+from .utils import get_parser_entry, save_parser_entry
 
 
 class FLParser:
@@ -47,48 +48,48 @@ class FLParser:
         """GET project page data => build data => return Data()"""
         # init named tuple
         Data = namedtuple(
-            "Data", ["id", "url", "title", "description", "budget", "deadline"]
+            "Data",
+            [
+                "pid",
+                "url",
+                "title",
+                "description",
+                "budget",
+                "deadline",
+                "source",
+                "sent",
+            ],
         )
+        # skip if object already exist - prevent target request
+        pid = f"{self.source}-{info.id}"
+        if get_parser_entry(pid):
+            return
         # create session
         session = HTMLSession()
         response = session.get(info.url)
         # parse html
-        title = response.html.find(".b-page__title", first=True).text.lower()
-        description = (
-            response.html.find(f"#projectp{info.id}", first=True)
-            .text.replace("\n", " ")
-            .lower()
-        )
+        title = response.html.find(".b-page__title", first=True).text.capitalize()
+        description = response.html.find(
+            f"#projectp{info.id}", first=True
+        ).text.replace("\n", " ")
         budget_deadline = response.html.find(".b-layout__txt span.b-layout__bold")
         budget = budget_deadline[0].text.lower() if len(budget_deadline) >= 1 else ""
         deadline = budget_deadline[1].text.lower() if len(budget_deadline) >= 2 else ""
         # build data
         project_data = Data(
-            id=info.id,
+            pid=pid,
             url=info.url,
             title=title,
             description=description,
             budget=budget,
             deadline=deadline,
-        )
-        return project_data
-
-    def save_project_data(self, data: namedtuple) -> None:
-        """Save ParserEntry data if not exist"""
-        print("Saving: " + data.title)
-        ParserEntry.objects.get_or_create(
-            pid=f"{self.source}-{data.id}",
-            url=data.url,
-            title=data.title,
-            description=data.description,
-            budget=data.budget,
-            deadline=data.deadline,
             source=self.source,
             sent=False,
         )
+        return project_data
 
     def run(self):
         projects_info = self.get_projects_info()
-        for info in projects_info:
+        for info in projects_info[:8]:
             project_data = self.get_project_data(info)
-            self.save_project_data(project_data)
+            save_parser_entry(project_data)
--- telegramservice/core/utils.py
+++ telegramservice/core/utils.py
@@ -1,7 +1,29 @@
+from collections import namedtuple
+
 from django.utils import timezone
+
+from .models import ParserEntry
 
 
 def datetime_days_ahead(days: int) -> timezone:
     now = timezone.now()
     delta = timezone.timedelta(days=days)
     return now + delta
+
+
+def save_parser_entry(data: namedtuple) -> None:
+    """Save ParserEntry data if not exist"""
+    if not data:
+        return
+    print("Saving: " + data.title)
+    data_dict = data._asdict()
+    ParserEntry.objects.get_or_create(**data_dict)
+
+
+def get_parser_entry(pid: str) -> ParserEntry:
+    """Get parser_entry or None"""
+    try:
+        parser_entry = ParserEntry.objects.get(pid=pid)
+        return parser_entry
+    except ParserEntry.DoesNotExist:
+        return None
