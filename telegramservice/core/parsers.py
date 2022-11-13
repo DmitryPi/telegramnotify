f20516f1d7fec3ade06a8eeee9374cf636d53eea
Dmitry G <sdprecovery@gmail.com>
2022-11-13 14:48:15 +0300

    update: FLParser get_project_data()

--- telegramservice/core/parsers.py
+++ telegramservice/core/parsers.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 from environ import Env
 from requests_html import HTMLSession
 
-from telegramservice.core.models import Target
+from .models import Target
 
 
 class FLParser:
@@ -38,19 +38,47 @@ class FLParser:
             proj_id = int(proj.attrs["href"].split("/")[2])
             # https://www.fl.ru/ + projects/<id>/<slug>
             proj_url = self.target.url_body + proj.attrs["href"][1:]
-            proj_info = Info(proj_id, proj_url)
+            proj_info = Info(id=proj_id, url=proj_url)
             projects_info.append(proj_info)
         return projects_info
 
-    def get_projects_data(self) -> dict:
-        """GET project page data => build data => return dict"""
-        project_data = {}
+    def get_project_data(self, info: (int, str)) -> namedtuple:
+        """GET project page data => build data => return Data()"""
+        # init named tuple
+        Data = namedtuple(
+            "Data", ["id", "url", "title", "body", "budget", "deadline", "sent"]
+        )
+        # create session
+        session = HTMLSession()
+        response = session.get(info.url)
+        # parse html
+        title = response.html.find(".b-page__title", first=True).text.lower()
+        body = (
+            response.html.find(f"#projectp{info.id}", first=True)
+            .text.replace("\n", " ")
+            .lower()
+        )
+        budget_deadline = response.html.find(".b-layout__txt span.b-layout__bold")
+        budget = budget_deadline[0].text.lower() if len(budget_deadline) >= 1 else ""
+        deadline = budget_deadline[1].text.lower() if len(budget_deadline) >= 2 else ""
+        # build data
+        project_data = Data(
+            id=info.id,
+            url=info.url,
+            title=title,
+            body=body,
+            budget=budget,
+            deadline=deadline,
+            sent=False,
+        )
         return project_data
 
-    def save_project_data(self) -> None:
+    def save_project_data(self, data: namedtuple) -> None:
         """Save project data if not exist"""
         pass
 
     def run(self):
-        info = self.get_projects_info()
-        print(info[0].url)
+        projects_info = self.get_projects_info()
+        project_data = self.get_project_data(projects_info[5])
+        print(project_data)
+        self.save_project_data(project_data)
