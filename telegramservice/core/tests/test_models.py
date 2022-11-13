0c2aef986ac3ead06a0626e1de3120d0a62d222f
Dmitry G <sdprecovery@gmail.com>
2022-11-13 19:08:19 +0300

    TestParserEntry

--- telegramservice/core/tests/test_models.py
+++ telegramservice/core/tests/test_models.py
@@ -1,7 +1,9 @@
+import re
+
 from django.test import TestCase
 
-from ..models import Order
-from .factories import OrderFactory
+from ..models import Order, ParserEntry
+from .factories import OrderFactory, ParserEntryFactory
 
 
 class TestOrder(TestCase):
@@ -27,3 +29,37 @@ class TestOrder(TestCase):
             assert obj.total_amount >= 0
             assert obj.telegram_payment_charge_id
             assert obj.provider_payment_charge_id
+
+
+class TestParserEntry(TestCase):
+    def setUp(self):
+        self.batch_size = 10
+        self.objects = ParserEntryFactory.create_batch(size=self.batch_size)
+
+    def test_create(self):
+        assert len(self.objects) == self.batch_size
+
+    def test_update(self):
+        new_title = "new title"
+        for obj in self.objects:
+            obj.title = new_title
+            obj.save()
+        for obj in self.objects:
+            assert obj.title == new_title
+
+    def test_delete(self):
+        for obj in self.objects:
+            obj.delete()
+        qs = ParserEntry.objects.all()
+        assert not len(qs)
+
+    def test_fields(self):
+        for obj in self.objects:
+            assert re.match(r"^(.*)-(\d+)$", obj.pid)  # match Fl.ru-123
+            assert len(obj.title) > 10
+            assert len(obj.description) > 10
+            assert obj.budget
+            assert obj.deadline
+            assert obj.url
+            assert obj.source == "FL.ru"
+            assert isinstance(obj.sent, bool)
