aa929a519efa6b8be8e126a315f4e90cc0ef27f4
Dmitry G <sdprecovery@gmail.com>
2022-11-13 14:25:12 +0300

    Refactor: get_projects_info

--- telegramservice/core/parsers.py
+++ telegramservice/core/parsers.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 from environ import Env
 from requests_html import HTMLSession
 
@@ -19,28 +21,36 @@ class FLParser:
             print("Target you are trying to find DoesNotExist")
             raise e
 
-    def get_projects_info(self) -> list[[int, str]]:
+    def get_projects_info(self) -> [(int, str)]:
         """GET target page => parse project id, build url
 
-        >> return [[proj_id, proj_url]]
+        >> return [Info(proj_id, proj_url)]
         """
+        # init named tuple
+        Info = namedtuple("Info", ["id", "url"])
+        # create session
         session = HTMLSession()
         response = session.get(self.target.url_query)
-        projects = response.html.find(".b-post__link")  # <a> proj link
+        # find <a> proj link
+        projects = response.html.find(".b-post__link")
         projects_info = []
         for proj in projects:
             proj_id = int(proj.attrs["href"].split("/")[2])
             # https://www.fl.ru/ + projects/<id>/<slug>
             proj_url = self.target.url_body + proj.attrs["href"][1:]
-            projects_info.append([proj_id, proj_url])
+            proj_info = Info(proj_id, proj_url)
+            projects_info.append(proj_info)
         return projects_info
 
     def get_projects_data(self) -> dict:
-        pass
+        """GET project page data => build data => return dict"""
+        project_data = {}
+        return project_data
 
     def save_project_data(self) -> None:
         """Save project data if not exist"""
         pass
 
     def run(self):
-        print(self.get_projects_info())
+        info = self.get_projects_info()
+        print(info[0].url)
