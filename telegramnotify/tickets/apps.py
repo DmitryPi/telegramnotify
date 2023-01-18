2e317431f802e1c75e71f21f3ee1337c223633e7
Dmitry G <sdprecovery@gmail.com>
2023-01-18 18:22:08 +0300

    Cleanup: new apps

--- telegramnotify/tickets/apps.py
+++ telegramnotify/tickets/apps.py
@@ -3,4 +3,10 @@ from django.apps import AppConfig
 
 class TicketsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "tickets"
+    name = "telegram.notify.tickets"
+
+    def ready(self):
+        try:
+            import telegramnotify.tickets.signals  # noqa
+        except ImportError:
+            pass
