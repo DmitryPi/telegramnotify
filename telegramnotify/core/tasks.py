318c9dc2ecd164631a14f0e6ba7819c35b6bb144
Dmitry G <sdprecovery@gmail.com>
2023-01-22 11:24:40 +0300

    Update: core tasks - add atomic, retry logic

--- telegramnotify/core/tasks.py
+++ telegramnotify/core/tasks.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from django.contrib.auth import get_user_model
+from django.db import transaction
 from django.utils import timezone
 
 from config import celery_app
@@ -13,8 +14,8 @@ User = get_user_model()
 sender_bot = SenderBot()
 
 
-@celery_app.task()
-def users_update_premium_expired_task():
+@celery_app.task(bind=True, max_retries=3, default_retry_delay=60)
+def users_update_premium_expired_task(self):
     """
     Loop through users
     Skip users with premium_status=permanent
@@ -22,23 +23,31 @@ def users_update_premium_expired_task():
         set premium_status=expired
     TODO: make it dumb
     """
-    users = get_users_exclude_expired()
-    for user in users:
-        if user.premium_status == User.PremiumStatus.permanent:
-            continue
-        if timezone.now() > user.premium_expire:
-            user.premium_status = User.PremiumStatus.expired
-            user.save(update_fields=["premium_status"])
-            asyncio.run(
-                sender_bot.raw_send_message(
-                    user.tg_id, sender_bot.premium_expired_message
-                )
-            )
+    with transaction.atomic():
+        try:
+            users = get_users_exclude_expired()
+            for user in users:
+                if user.premium_status == User.PremiumStatus.permanent:
+                    continue
+                if timezone.now() > user.premium_expire:
+                    user.premium_status = User.PremiumStatus.expired
+                    user.save(update_fields=["premium_status"])
+                    asyncio.run(
+                        sender_bot.raw_send_message(
+                            user.tg_id, sender_bot.premium_expired_message
+                        )
+                    )
+        except Exception as exc:
+            raise self.retry(exc=exc)
 
 
-@celery_app.task()
-def users_update_wallet_decrement_by_pay_rate_task():
+@celery_app.task(bind=True, max_retries=3, default_retry_delay=60)
+def users_update_wallet_decrement_by_pay_rate_task(self):
     """Dumb task: to loop over users and call user_wallet_decrement_by_pay_rate()"""
-    users = get_users_exclude_expired()
-    for user in users:
-        user_wallet_decrement_by_pay_rate(user)
+    with transaction.atomic():
+        try:
+            users = get_users_exclude_expired()
+            for user in users:
+                user_wallet_decrement_by_pay_rate(user)
+        except Exception as exc:
+            raise self.retry(exc=exc)
